de60b011dbe82656fadf978fed1cae8dc39cb299
jaypalapar <palaparjay15@gmail.com>
2022-07-06 22:45:15 +0800

    Design Updated

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -4,21 +4,33 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+            <div class="card verification">
+                <div class="col-md-12">
+                    <img class="verify-logo" src="{{ asset('assets/img/verified/verified.svg') }}" alt="verified">
+                </div>
 
-                <div class="card-body">
+                <h1 class="align-center verify-title">{{ __('Verify Your Email Address') }}</h1>
+
+                <div class="card-body align-center">
+
                     @if (session('resent'))
                         <div class="alert alert-success" role="alert">
                             {{ __('A fresh verification link has been sent to your email address.') }}
                         </div>
                     @endif
 
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }}, <a href="{{ route('verification.resend') }}">{{ __('click here to request another') }}</a>.
+                    <p>{{ __('Before proceeding, please check your email for a verification link.') }}</p>
+                    <p>{{ __('If you did not receive the email, please click the button below.') }}</p>
                 </div>
+
+                <div class="buttons justify-content-center">
+                    <button type="submit" class="button is-solid primary-button raised" href="{{ route('verification.resend') }}">
+                        {{ __('Request Another') }}
+                    </button>
+                </div>
+
             </div>
         </div>
     </div>
 </div>
-@endsection
+@endsection 
