4ed54549486b45012a3662ca7a68da9fcf65e3b6
RDMontana06 <montanareymart06@gmail.com>
2022-11-29 22:38:31 +0800

    Remove QR Scanner

--- config/app.php
+++ config/app.php
@@ -227,7 +227,6 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Alert' => RealRashid\SweetAlert\Facades\Alert::class,
-        'QrCode' => SimpleSoftwareIO\QrCode\Facades\QrCode::class,
 
     ],
 
